3286d9af0cc9d06929070a1b072b69468e0b9732
Tom Stratton <tstratton3912@gmail.com>
2020-08-22 11:30:38 +0100

    urlencode

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,6 +1,13 @@
 <?php
 
+require_once('../../../private/initialize.php'); 
+
 $id = $_GET['id'] ?? '1';
 echo $id;
 
 ?>
+
+
+<a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a><br />
+<a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a><br />
+<a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a><br />
